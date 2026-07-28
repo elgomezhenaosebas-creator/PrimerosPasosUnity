60b7fe27a4bbd3ad625db748a309e782dc5de605
pache739 <juansebastianpatinomoncada@gmail.com>
2026-07-28 11:47:07 -0500

    Pachinco e intro a C#

---
+++ MyUnity/Assets/_Main/Scripts/Player.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.log("star");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.log("Update");
+    }
+}
